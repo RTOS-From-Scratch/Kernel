6442a45dcf58f7d6c26b01ed528f64446820519d
Baron Leonardo <mohamedayman.fcis@zoho.com>
2017-03-04 20:06:34 +0200

    add  to

--- startup.c
+++ startup.c
@@ -37,6 +37,7 @@ static uint32_t pui32Stack[128];
 //*****************************************************************************
 // To be added by user
 extern void SysTickHandler(void);
+extern void PendSVHandler(void);
 
 //*****************************************************************************
 //
@@ -63,7 +64,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // SVCall handler
     IntDefaultHandler,                      // Debug monitor handler
     0,                                      // Reserved
-    IntDefaultHandler,                      // The PendSV handler
+    PendSVHandler,                          // The PendSV handler
     SysTickHandler,                      	// The SysTick handler
     IntDefaultHandler,                      // GPIO Port A
     IntDefaultHandler,                      // GPIO Port B
