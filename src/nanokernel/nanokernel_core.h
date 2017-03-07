91a403be27bacfd031aac3ed87a34c7438983373
Baron Leonardo <mohamedayman.fcis@zoho.com>
2017-03-08 00:19:00 +0200

    delete unused files

--- src/nanokernel/nanokernel_core.h
+++
@@ -1,7 +0,0 @@
-#ifndef NANOKERNEL_CORE_H_
-#define NANOKERNEL_CORE_H_
-
-typedef struct nanokernel_core_TCB nanokernel_core_TCB;
-extern nanokernel_core_TCB *core_TCB;
-
-#endif // NANOKERNEL_CORE_H_
